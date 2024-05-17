728a4a42be7a45dcfa13418ef75c0a63061a35fd
alex <akadlofa@gmail.com>
2024-05-17 11:58:51 -0400

    removed bootstrap references, added tailwind

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './static/css/index.css';
 import './static/css/colors.css';
-import "bootstrap/dist/css/bootstrap.min.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- frontend/src/static/js/serviceworker.js
+++ frontend/src/static/js/serviceworker.js
@@ -1,9 +1,9 @@
 function log(...data) {
     console.log("SWv1.0", ...data);
-  }
-  
+}
+
 log("SW Script executing - adding event listeners");
-  
+
 
 const CACHE_NAME = 'foodfolio-app-cache';
 const urlsToCache = [
@@ -42,13 +42,12 @@ const urlsToCache = [
     '../../components/map/restaurantMarker.js',
     'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
     'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
-    'https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css'
 ];
 
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
-        return cache.addAll(urlsToCache);
+            return cache.addAll(urlsToCache);
         })
     );
 });
@@ -56,9 +55,9 @@ self.addEventListener('install', (event) => {
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
+            return response || fetch(event.request);
         }).catch(() => {
-            return caches.match('/offline'); 
+            return caches.match('/offline');
         })
     );
 });
@@ -68,14 +67,14 @@ self.addEventListener('activate', (event) => {
 
     event.waitUntil(
         caches.keys().then((cacheNames) => {
-        return Promise.all(
-            cacheNames.map((cacheName) => {
-            if (!cacheWhitelist.includes(cacheName)) {
-                return caches.delete(cacheName);
-            }
-            return null;
-            })
-        );
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                    return null;
+                })
+            );
         })
     );
 });
@@ -91,21 +90,21 @@ const submitFormDataWhenOnline = () => {
         // User is online, submit the form data to the API
         const formData = getFormData(); // Implement your form data retrieval logic
         if (formData) {
-        // Assuming there's an API endpoint to submit the form data
-        fetch('https://api.example.com/submit', {
-            method: 'POST',
-            headers: {
-            'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(formData),
-        })
-            .then((response) => response.json())
-            .then((data) => {
-            console.log('Form data submitted successfully:', data);
+            // Assuming there's an API endpoint to submit the form data
+            fetch('https://api.example.com/submit', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
             })
-            .catch((error) => {
-            console.error('Error submitting form data:', error);
-            });
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log('Form data submitted successfully:', data);
+                })
+                .catch((error) => {
+                    console.error('Error submitting form data:', error);
+                });
         }
     } else {
         console.log('User is offline. Storing form data locally for later submission.');
