b3a4c75e4e6c9b7372ed2c40f733ac854eb528fd
aka-BAT <aka@bausch-group.com>
2024-06-27 11:38:50 -0400

    star rating sizing bug fixed

--- frontend/src/components/starRating.js
+++ frontend/src/components/starRating.js
@@ -4,18 +4,14 @@ import styles from "../static/css/star-rating.module.css"
  * 
  * @param {number} value 1-5 depending on number of stars
  * @param {*} onChange callback for when the value changes which takes a value as a param
- * @param {1 | 2 | 3 | 4 | 5 | 6} size the size of this element, the bigger the number the larger
+ * @param size the size of this element in pixels (multiplied by 5)
  * @param {boolean} readonly if true, there will be no hover effect, and the star rating cannot be changed, if false, the user can interact with the rating
  * @returns 
  */
 const StarRating = ({ value, onChange, size, readonly }) => {
-    if (size < 1 || size > 6) {
-        throw new Error("StarRating size must fall in the range of 1-6 (inclusive).");
-    }
-
     const Star = ({ filled, onClick }) => {
         const starSymbol = filled ? '\u2605' : '\u2606';
-        return <span className={`${styles.star} h${7 - size}`} onClick={onClick}>{starSymbol}</span>;
+        return <span className={`${styles.star}`} onClick={onClick} style={{ fontSize: `${size * 5}px` }}>{starSymbol}</span>;
     };
 
     const handleClick = (starIndex) => {
